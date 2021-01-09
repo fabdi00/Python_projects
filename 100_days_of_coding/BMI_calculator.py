4dbf090770cc346fc75234852f9b68d97fc6bea0
faisal <faisal@MacBook-Pro.local>
2021-01-09 18:06:50 +0000

    f

---
+++ 100_days_of_coding/BMI_calculator.py
@@ -0,0 +1,12 @@
+100 days of coding: Day 2 challenge
+
+Calculate your BMI
+
+
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+
+h = float(height)**2
+w = int(weight)
+bmi = w / h
+print(int(bmi))
